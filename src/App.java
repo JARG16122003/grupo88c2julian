026ff0aab9f97da4c56f5e3e45c0ae76d3335b94
JARG16122003 <garciajulian1612@gmail.com>
2021-07-04 18:14:42 -0500

    Java lang lessons

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
 public class App {
+
+    private String nombre;
+    private int identificacion;
+    private double estatura;
+    private float peso;
+    private char sexo;
+    private boolean estaVacunado;
+
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
 }
+
+
